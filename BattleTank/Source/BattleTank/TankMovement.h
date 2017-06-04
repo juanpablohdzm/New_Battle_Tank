dfcf3ae35c534afda3185dd5c9221d3879d200d4
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-06-04 12:01:22 -0500

    Tank Movement

---
+++ BattleTank/Source/BattleTank/TankMovement.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovement : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
--- BattleTank/Source/BattleTank/TankTrack.cpp
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -11,8 +11,8 @@ void UTankTrack::SetThrottle(float Throttle)
 	FVector ForceApplied = GetForwardVector()*Throttle * TrackMaxDrivingForce;
 	FVector ForceLocation = GetComponentLocation();
 		
-			UPrimitiveComponent * TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-			TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	UPrimitiveComponent * TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 	
 		
 }
